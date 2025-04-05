ce0bbcaa36b1f0c035f61dcdcdee5ecacf58f37b
Kumar <2401688@cognizant.com>
2025-04-05 21:48:20 +0530

    LFS

---
+++ codeFirst/codeFirst/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace codeFirst.Models
+{
+    public class Course
+    {
+        [Key]
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+
+        public int CourseDuration { get; set;}
+
+        public decimal CourseFees { get; set;}
+    }
+}
---
+++ codeFirst/codeFirst/Models/CourseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace codeFirst.Models
+{
+    public class CourseContext: DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+        public CourseContext(DbContextOptions<CourseContext> options):base (options)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("data source=LTIN617499; database=CodeFirst;integrated security=true;trustservercertificate=true;");
+        }
+    }
+}
